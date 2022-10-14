9a0281a23df14f22ef4b102c0089940406aae01b
TanmayGoregaonkar <93464019+TanmayGoregaonkar@users.noreply.github.com>
2022-10-14 15:52:52 +0530

    Add files via upload

---
+++ bankers.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    // P0, P1, P2, P3, P4 are the Process names here
+
+    int n, m, i, j, k;
+    // n =  Number of processes
+    // m =  Number of resources
+
+    printf("Enter no of processses\n");
+    scanf("%d", &n);
+    printf("Enter no of resources\n");
+    scanf("%d", &m);
+
+    int alloc[n][m];
+    int max[n][m];
+    // int avail[3] = {3, 3, 2}; // Available Resources
+    int avail[m];
+    
+    printf("Enter allocated values\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &alloc[i][j]);
+        }
+    }
+
+    printf("Enter max values\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &max[i][j]);
+        }
+    }
+
+    printf("Enter available values\n");
+      for (int i = 0; i < m; i++)
+    {
+        scanf("%d",&avail[i]);
+    }
+        int f[n], ans[n], ind = 0;
+        for (k = 0; k < n; k++)
+        {
+            f[k] = 0;
+        }
+        int need[n][m];
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < m; j++)
+                need[i][j] = max[i][j] - alloc[i][j];
+        }
+        int y = 0;
+        for (k = 0; k < 5; k++)
+        {
+            for (i = 0; i < n; i++)
+            {
+                if (f[i] == 0)
+                {
+                    int flag = 0;
+                    for (j = 0; j < m; j++)
+                    {
+                        if (need[i][j] > avail[j])
+                        {
+                            flag = 1;
+                            break;
+                        }
+                    }
+
+                    if (flag == 0)
+                    {
+                        ans[ind++] = i;
+                        for (y = 0; y < m; y++)
+                            avail[y] += alloc[i][y];
+                        f[i] = 1;
+                    }
+                }
+            }
+        }
+
+        int flag = 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (f[i] == 0)
+            {
+                flag = 0;
+                printf("The following system is not safe");
+                break;
+            }
+        }
+
+        if (flag == 1)
+        {
+            printf("Following is the SAFE Sequence\n");
+            for (i = 0; i < n - 1; i++)
+                printf(" P%d ->", ans[i]);
+            printf(" P%d", ans[n - 1]);
+        }
+        printf("\n");
+
+        return (0);
+    }
+
